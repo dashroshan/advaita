e16459c2bd5a0abd3c1f80cf3ba954156a55578f
Roshan Dash <roshan1337d@gmail.com>
2023-01-15 00:16:58 +0530

    loader, home page slider, video

--- index.js
+++ index.js
@@ -31,3 +31,38 @@ window.onscroll = function () {
     prevScrollpos = currentScrollPos;
 }
 // Nav bar end
+
+
+
+// Events Start
+var swiper = new Swiper(".mySwiper", {
+    breakpoints: {
+        945: { slidesPerView: 3 },
+        630: { slidesPerView: 2, spaceBetween: 25 },
+    },
+    spaceBetween: 10,
+    slidesPerView: 1,
+    centeredSlides: false,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+});
+// Events end
+
+
+const pageLoaded = () => {
+    // Remove loader
+    const loader = document.getElementById('loader_block');
+    loader.style.opacity = '0';
+    setTimeout(() => loader.style.display = 'none', 300);
+
+    window.removeEventListener('load', pageLoaded);
+}
+window.addEventListener('load', pageLoaded);
